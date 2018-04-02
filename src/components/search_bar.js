07005e622a70b0604d39627f3464f4fcd6105987
Atsushi Neki <neki.atsushi@jp.fujitsu.com>
2018-04-03 09:24:05 +1000

    Initial lesson

---
+++ src/components/search_bar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+// import ReactDom from 'react-dom';
+
+class SearchBar extends Component{
+    render(){
+        return <input onChange={(event) => console.log(event.target.value)}/>;
+    }
+}
+
+export default SearchBar;
