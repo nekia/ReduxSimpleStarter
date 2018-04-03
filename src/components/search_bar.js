e820d7c6e94ff52bc4a3c163dca4034ee748d301
Atsushi Neki <neki.atsushi@jp.fujitsu.com>
2018-04-03 11:20:29 +1000

    Add a constructo

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 // import ReactDom from 'react-dom';
 
 class SearchBar extends Component{
+    constructor(props){
+        super(props);
+        
+        this.state = { term: '' };
+    }
     render(){
         return <input onChange={(event) => console.log(event.target.value)}/>;
     }
